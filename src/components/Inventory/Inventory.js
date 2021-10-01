dc5ebf6fae592b2a1e7464eab6f7d055968a4564
unknown <anwarhosen575@gmail.com>
2021-10-01 09:01:21 +0600

    raja

---
+++ src/components/Inventory/Inventory.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Inventory = () => {
+    return (
+        <div>
+            <h2>this is inventory</h2>
+            <p>raja</p>
+        </div>
+    );
+};
+
+export default Inventory;
